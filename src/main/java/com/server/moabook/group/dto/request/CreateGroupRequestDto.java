1e07fb4121932c94a709a033f6c9238fe3bffae7
jwj9127 <jwj9127@naver.com>
2025-02-23 00:50:59 +0900

    ✨ [feat]: DTO 필드 추가
    - color 필드 추가

--- src/main/java/com/server/moabook/group/dto/request/CreateGroupRequestDto.java
+++ src/main/java/com/server/moabook/group/dto/request/CreateGroupRequestDto.java
@@ -4,6 +4,9 @@ import jakarta.validation.constraints.NotNull;
 
 public record CreateGroupRequestDto(
         @NotNull(message = "그룹의 이름이 비어있을 수 없습니다.")
-        String name
+        String name,
+
+        @NotNull(message = "색상이 비어있을 수 없습니다.")
+        String color
 ) {
 }
