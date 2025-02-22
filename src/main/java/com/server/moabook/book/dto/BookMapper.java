5a1478bff27d18e2e2947cd575ff0839953b0b1a
jwj9127 <jwj9127@naver.com>
2025-02-23 03:41:45 +0900

    ✨ [feat]: Mapper 추가
    BOOK Mapper 추가

---
+++ src/main/java/com/server/moabook/book/dto/BookMapper.java
@@ -0,0 +1,32 @@
+package com.server.moabook.book.dto;
+
+import com.server.moabook.book.domain.Book;
+import com.server.moabook.book.dto.request.CreateBookRequestDto;
+import com.server.moabook.book.dto.request.UpdateBookRequestDto;
+import com.server.moabook.book.dto.response.SelectBookResponseDto;
+import com.server.moabook.group.domain.Group;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class BookMapper {
+
+    public static Book toEntity(CreateBookRequestDto createBookRequestDto, Group group) {
+        return Book.builder()
+                .name(createBookRequestDto.name())
+                .group(group)
+                .build();
+    }
+
+    public static SelectBookResponseDto toDTO(List<Book> books) {
+        List<BookDto> bookDtos = books.stream()
+                .map(book -> new BookDto(book.getId(), book.getName()))
+                .collect(Collectors.toList());
+        return new SelectBookResponseDto(bookDtos);
+    }
+
+    public static void update(Book book, UpdateBookRequestDto updateBookRequestDto) {
+        book.setName(updateBookRequestDto.name());
+    }
+
+}
