328d0670f212b1bd500d508c546a831c3703322a
jwj9127 <jwj9127@naver.com>
2025-02-24 08:27:55 +0900

    ✨ [feat]: Message 추가
    유저, 페이지 관련 메시지 추가

--- src/main/java/com/server/moabook/global/exception/message/ErrorMessage.java
+++ src/main/java/com/server/moabook/global/exception/message/ErrorMessage.java
@@ -12,6 +12,7 @@ public enum ErrorMessage {
     INVALID_JWT_TOKEN(HttpStatus.UNAUTHORIZED.value(), "유효하지 않은 accessToken입니다."),
     GROUP_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "해당 그룹을 찾을 수 없습니다."),
     BOOK_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "해당 책을 찾을 수 없습니다."),
+    PAGE_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "해당 책을 찾을 수 없습니다."),
     USER_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "사용자를 찾을 수 없습니다.");
     private final int code;
     private final String message;
--- src/main/java/com/server/moabook/global/exception/message/SuccessMessage.java
+++ src/main/java/com/server/moabook/global/exception/message/SuccessMessage.java
@@ -9,6 +9,9 @@ import org.springframework.http.HttpStatus;
 public enum SuccessMessage {
 
     SIGNIN_SUCCESS(HttpStatus.CREATED.value(),"로그인에 성공하였습니다."),
+    SELECT_USER_SUCCESS(HttpStatus.CREATED.value(),"유저 조회에 성공하였습니다."),
+    DELETE_USER_SUCCESS(HttpStatus.CREATED.value(),"유저 삭제에 성공하였습니다."),
+    DELETE_ALL_USER_SUCCESS(HttpStatus.CREATED.value(),"유저 기록 삭제에 성공하였습니다."),
     CREATE_GROUP_SUCCESS(HttpStatus.OK.value(),"그룹 생성을 성공하였습니다."),
     SELECT_GROUP_SUCCESS(HttpStatus.OK.value(),"그룹 조회를 성공하였습니다."),
     UPDATE_GROUP_SUCCESS(HttpStatus.OK.value(),"그룹 수정을 성공하였습니다."),
@@ -16,7 +19,11 @@ public enum SuccessMessage {
     CREATE_BOOK_SUCCESS(HttpStatus.OK.value(),"책 생성을 성공하였습니다."),
     SELECT_BOOK_SUCCESS(HttpStatus.OK.value(),"책 조회를 성공하였습니다."),
     UPDATE_BOOK_SUCCESS(HttpStatus.OK.value(),"책 수정을 성공하였습니다."),
-    DELETE_BOOK_SUCCESS(HttpStatus.OK.value(),"책 삭제를 성공하였습니다.");
+    DELETE_BOOK_SUCCESS(HttpStatus.OK.value(),"책 삭제를 성공하였습니다."),
+    CREATE_PAGE_SUCCESS(HttpStatus.OK.value(),"페이지 생성을 성공하였습니다."),
+    SAVE_PAGE_SUCCESS(HttpStatus.OK.value(),"페이지 저장을 성공하였습니다."),
+    SELECT_PAGE_SUCCESS(HttpStatus.OK.value(),"페이지 조회를 성공하였습니다."),
+    DELETE_PAGE_SUCCESS(HttpStatus.OK.value(),"페이지 삭제를 성공하였습니다.");
 
     private final int code;
     private final String message;
