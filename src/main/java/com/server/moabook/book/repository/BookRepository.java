a5c9b0922aa3ed5003954095d21c1b017cf8e104
jwj9127 <jwj9127@naver.com>
2025-02-24 21:24:52 +0900

    [fix]: Repository 수정

--- src/main/java/com/server/moabook/book/repository/BookRepository.java
+++ src/main/java/com/server/moabook/book/repository/BookRepository.java
@@ -1,12 +1,10 @@
 package com.server.moabook.book.repository;
 
 import com.server.moabook.book.domain.Book;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
-    List<Book> findByGroup_Id(Long groupId);
 }
