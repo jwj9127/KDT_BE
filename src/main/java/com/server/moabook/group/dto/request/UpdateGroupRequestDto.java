545cd62c68e653b77cf761a77329347b52ccad7e
jwj9127 <jwj9127@naver.com>
2025-02-28 02:40:33 +0900

    feat : dto 추가 - color dto 추가

--- src/main/java/com/server/moabook/group/dto/request/UpdateGroupRequestDto.java
+++ src/main/java/com/server/moabook/group/dto/request/UpdateGroupRequestDto.java
@@ -6,6 +6,8 @@ public record UpdateGroupRequestDto(
         @NotNull(message = "그룹의 아이디는 비어있을 수 없습니다.")
         Long groupId,
         @NotNull(message = "그룹의 이름이 비어있을 수 없습니다.")
-        String name
+        String name,
+        @NotNull(message = "색상의 이름은 비어있을 수 없습니다.")
+        String color
 ) {
 }
